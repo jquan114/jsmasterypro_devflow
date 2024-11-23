c2ce4562e9b066502f007c99a2f9c40be6ce31eb
jquan <jquancollins96@gmail.com>
2024-11-23 14:42:47 -0500

    eslint & prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-
-
 const Home = () => {
   return (
     <div>
-    <h1 className= 'text-3xl text-violet-700'>DevFlow</h1>
-  
-  </div>
-  )
-   
-
-}
+      <h1 className="text-3xl text-violet-700">DevFlow</h1>
+    </div>
+  );
+};
 
 export default Home;
