f1c7dc571650ec073de24024489afd50aaab8c13
jquan <jquancollins96@gmail.com>
2024-11-23 15:49:33 -0500

    tailwind css setup and theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-violet-700">DevFlow</h1>
+      <h1 className=" h1-bold  background-light850_dark100">
+        Welcome to the world of next js
+      </h1>
     </div>
   );
 };
