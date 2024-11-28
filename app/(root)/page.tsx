86f6c5fe3b2de867d2c96165618aa21ec21057ee
jquan <jquancollins96@gmail.com>
2024-11-27 19:05:27 -0500

    MobileNav & NavLinks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
@@ -11,15 +9,6 @@ const Home = async () => {
       <h1 className=" h1-bold  background-light850_dark100">
         Welcome to the world of next js
       </h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit"> Log Out In Jesus Name </Button>
-      </form>
     </>
   );
 };
--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Theme from "./Theme";
+import MobileNavigation from "./MobileNavigation";
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
@@ -19,6 +20,7 @@ const Navbar = () => {
       <p>Global Search</p>
       <div className="flex-between gap-5">
         <Theme />
+        <MobileNavigation />
       </div>
     </nav>
   );
