2268a9bca9eed7412b09dde7ae7a2094cfc3b31d
jquan <jquancollins96@gmail.com>
2024-12-05 13:40:59 -0500

    Home Page Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,28 @@
-import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
 
 const Home = async () => {
-  const session = await auth();
-
-  console.log(session);
   return (
     <>
-      <h1 className=" h1-bold  background-light850_dark100">
-        A place where christians can unite as one.
-      </h1>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}> ASK A QUESTION</Link>
+        </Button>
+      </section>
+      <section className="mt-11">LocalSearch</section>
+      Home Filter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p> Question card 1</p>
+        <p> Question card 2</p>
+        <p> Question card 3</p>
+        <p> Question card 4</p>
+      </div>
     </>
   );
 };
