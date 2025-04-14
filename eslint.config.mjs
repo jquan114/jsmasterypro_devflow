3c74bb268167117d4ab25901a6772359539b043c
jquan <jquancollins96@gmail.com>
2025-04-13 21:55:37 -0400

    Migrate to esLint9 Correction

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,71 +1,63 @@
+import { defineConfig } from "eslint/config";
+import _import from "eslint-plugin-import";
+import { fixupPluginRules } from "@eslint/compat";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-
-import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
 });
 
-const config = [
-  {
-    ignores: ["components/ui/**/*"],
-  },
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "standard",
-    "plugin:tailwindcss/recommended",
-    "plugin:prettier/recommended",
-    "prettier"
-  ),
-  {
-    rules: {
-      "import/order": [
-        "error",
-        {
-          groups: [
-            "builtin",
-            "external",
-            "internal",
-            ["parent", "sibling"],
-            "index",
-            "object",
-          ],
+export default defineConfig([{
+    extends: compat.extends(
+        "next/core-web-vitals",
+        "next/typescript",
+        "standard",
+        "plugin:tailwindcss/recommended",
+        "prettier",
+    ),
 
-          "newlines-between": "always",
-
-          pathGroups: [
-            {
-              pattern: "@app/**",
-              group: "external",
-              position: "after",
-            },
-          ],
-
-          pathGroupsExcludedImportTypes: ["builtin"],
-
-          alphabetize: {
-            order: "asc",
-            caseInsensitive: true,
-          },
-        },
-      ],
-      "comma-dangle": "es5",
+    plugins: {
+        import: fixupPluginRules(_import),
     },
-  },
-  {
+
+    rules: {
+        "import/order": ["error", {
+            groups: [
+                "builtin",
+                "external",
+                "internal",
+                ["parent", "sibling"],
+                "index",
+                "object",
+            ],
+
+            "newlines-between": "always",
+
+            pathGroups: [{
+                pattern: "@app/**",
+                group: "external",
+                position: "after",
+            }],
+
+            pathGroupsExcludedImportTypes: ["builtin"],
+
+            alphabetize: {
+                order: "asc",
+                caseInsensitive: true,
+            },
+        }],
+    },
+}, {
     files: ["**/*.ts", "**/*.tsx"],
 
     rules: {
-      "no-undef": "off",
+        "no-undef": "off",
     },
-  },
-];
-
-export default config;
+}]);
